d9b13b974ad8c7a84a3849bcc113dbca39c58c98
Konovalov Andrey Sergeevich <andrrrey.konovalov@gmail.com>
2022-06-21 23:32:53 +0300

    Converter has taken functions from canvas Now all operations connected to convertation from canvas have moved to converter methods; the same thing is to happen to similar functions from Noise_eliminator. Note: the program compliles, but I haven't tested it's work yet!

---
+++ head/typedefs.hpp
@@ -0,0 +1,13 @@
+#ifndef TYPEDEFS_HPP_INCLUDED
+#define TYPEDEFS_HPP_INCLUDED
+
+#include <vector>
+#include "color.hpp"
+
+typedef std::vector<color> color_vec;
+typedef std::vector<color_vec> color_matr;
+typedef std::vector<float> float_vec;
+typedef std::vector<float_vec> float_matr;
+
+
+#endif
--- src/converter.cpp
+++ src/converter.cpp
@@ -1,3 +1,76 @@
 #include "../head/converter.hpp"
 
+#include <iostream>
+#include <cmath>
+
 // implementation
+
+void Converter::uns_chars_to_color_matr( const unsigned char * data, color_matr& cmatr, int C ){
+	
+	int H = cmatr.size(),
+	    W = cmatr[0].size();
+
+	try{
+		for(int i=0; i<H; i++){
+			for(int j=0; j<W; j++){
+				
+				if( C %2 ){
+					cmatr[i][j].alpha = 255;
+				}
+				else{
+					cmatr[i][j].alpha =
+						(int) data[ 2*(i*W + j) + C - 1 ];
+				}
+				
+				if( C > 2 ){
+					cmatr[i][j].r = (int) data[ C*(i*W+j)   ];
+					cmatr[i][j].g = (int) data[ C*(i*W+j)+1 ];
+					cmatr[i][j].b = (int) data[ C*(i*W+j)+2 ];
+				}
+				else{
+					int newcol = (int) data[ i*W + j ];
+					cmatr[i][j].r = newcol;
+					cmatr[i][j].g = newcol;
+					cmatr[i][j].b = newcol;
+				}
+			}
+		}
+	}
+	catch(...){
+		std::cout << "Couldn't convert char* to color-matrix!\n";
+	}
+}
+
+
+void Converter::color_matr_to_chars( const color_matr& cmatr, char * data, int C ){
+	
+	int H = cmatr.size(),
+	    W = cmatr[0].size();
+
+	try{
+		for(int i=0; i<H; i++){
+			for(int j=0; j<W; j++){
+				
+				if( !( C%2 ) ){
+					data[ C*(i*W +j) + C-1] = (char)(cmatr[i][j]).alpha;
+				}
+				
+				if( C > 2 ){
+					data[ C*(i*W + j) +0] = (char)(cmatr[i][j]).r;
+					data[ C*(i*W + j) +1] = (char)(cmatr[i][j]).g;
+					data[ C*(i*W + j) +2] = (char)(cmatr[i][j]).b;
+				}
+				else{
+					data[ C*(i*W + j)] = (char)( (int)round( (float)
+					  ( cmatr[i][j].r + cmatr[i][j].g + cmatr[i][j].b )/3 ) );
+				}
+			}
+		}
+	}
+	catch( ... ){
+		std::cout << "Couldn't convert color-matrix to char*!\n";
+	}
+}
+
+
+//
